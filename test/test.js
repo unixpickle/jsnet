5f92080898e422615b96ccea40ffe6b454b5c054
Alex Nichol <unixpickle@gmail.com>
2018-02-19 14:23:42 -0800

    detect nans in tests
    This makes the image patch test fail

--- test/test.js
+++ test/test.js
@@ -4,7 +4,9 @@ function assertClose(t1, t2, epsilon) {
     epsilon = epsilon || DEFAULT_EPSILON;
     const diff = t1.copy().sub(t2);
     for (var i = 0; i < diff.data.length; ++i) {
-        if (Math.abs(diff.data[i]) > epsilon) {
+        if (isNaN(diff.data[i])) {
+            throw Error('NaN in Tensor');
+        } else if (Math.abs(diff.data[i]) > epsilon) {
             throw Error('Tensors differ');
         }
     }
